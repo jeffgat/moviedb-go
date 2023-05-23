e31a83d5c0973b097213f5ed9661290150a8c1a8
jeffgat <jeff.gtbn@gmail.com>
2023-05-23 08:48:34 -0600

    fixed spinner position

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
         className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full"
       >
         {isLoading ? (
-          <div className="col-span-5 mt-40">
+          <div className="flex justify-center col-span-5 mt-40">
             <Spinner />
           </div>
         ) : (
